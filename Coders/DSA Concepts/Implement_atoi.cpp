feb06b8086e84105cc96464d7690519873d1a496
Sameer <116290562+codewithsameer@users.noreply.github.com>
2022-10-27 21:41:36 +0530

    Add files via upload

---
+++ Coders/DSA Concepts/Implement_atoi.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int val;
+    char strn1[] ="12546";
+ 
+    val = atoi(strn1);
+    cout <<"String value ="<<strn1 <<endl;
+    cout <<"Integer value ="<<val <<endl;
+ 
+    char strn2[] ="GeeksforGeeks";
+    val = atoi(strn2);
+    cout <<"String value ="<<strn2 <<endl;
+    cout <<"Integer value ="<<val <<endl;
+ 
+    return (0);
+}
+ 
